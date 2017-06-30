929a976e3ae590eb4804439f463d7aa188c4fcb6
Luke Fiji <luke.fiji@gmail.com>
2017-06-29 22:53:46 -0700

    Adding todos now possible

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { mongoose } = require("./db/mongoose");
+
+// Import models
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
+
+const app = express();
+
+// Use body-parser to send json to Express app
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  // Collect text data from body request
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  // Save todo
+  todo.save().then(
+    () => {
+      res.send(doc);
+    },
+    e => {
+      // Send an error with 400 status (Bad Request)
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
