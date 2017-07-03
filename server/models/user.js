b42f626625b30d872898e45f6f677580b975ab3d
Luke Fiji <luke.fiji@gmail.com>
2017-07-03 16:24:16 -0700

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -81,6 +82,24 @@ userSchema.statics.findByToken = function(token) {
     "tokens.access": "auth"
   });
 };
+
+// Middleware - before save
+userSchema.pre("save", function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    // Generate a 10-round salt /w bcrypt
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        // Set user password to generated hash
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 
